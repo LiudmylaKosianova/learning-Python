1d2a2c25d2d0655d6de94f21d02b1c4adde10090
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-05 13:35:48 +0200

    module3

--- openEDG/python-essentials-1/module3.py
+++ openEDG/python-essentials-1/module3.py
@@ -2,7 +2,7 @@
 reverse the elements in the list
 """
 
-list1 = [1, 22, 3, 44]
+list1 = [1, 2, 3, 4, 5, 6]
 length1 = len(list1)
 
 print(list1)
@@ -10,4 +10,25 @@ print(list1)
 for i in range(length1//2):
     list1[i], list1[length1-i-1] = list1[length1-i-1], list1[i]
 
-print("Reversed list:\n", list1)
+print("Reversed list:")
+print(list1)
+#list.reverse()
+
+"""
+Bubble sort. O(n^2).
+"""
+print("\n* * * Bubble sort * * *\n")
+print("List to be sorted:")
+print(list1)
+flag = True
+
+while flag:
+    flag = False
+    for i in range(len(list1)-1):
+        if list1[i] > list1[i+1]:
+            list1[i], list1[i+1] = list1[i+1], list1[i]
+            flag = True
+
+print("Sorted with Bubble sort")
+print(list1)
+#list.sort()
