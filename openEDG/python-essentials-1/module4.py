3936a97a1e6546ae631929e479d5b5cb6a1aacac
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-09 21:54:31 +0200

    module 4

--- openEDG/python-essentials-1/module4.py
+++ openEDG/python-essentials-1/module4.py
@@ -46,3 +46,23 @@ print(bmi(352.5, 1.65))
 
 isItOrdered = {1:"red", 2:"pink", 3:"white", 4:"black"}
 print(isItOrdered)
+
+"""
+exceptions
+
+try:
+	# It's a place where
+	# you can do something 
+    # without asking for permission.
+except:
+	# It's a spot dedicated to 
+    # solemnly begging for forgiveness.
+"""
+
+try:
+    value = int(input('Enter a natural number: '))
+    print('The reciprocal of', value, 'is', 1/value)        
+except ValueError:
+    print('I do not know what to do.')    
+except ZeroDivisionError:
+    print('Division by zero is not allowed in our Universe.') 
