e9eb17e14bfc3172309b92f72ecd6e7eff3f0404
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-09 11:49:01 +0200

    module 4 labs

--- openEDG/python-essentials-1/module4labs.py
+++ openEDG/python-essentials-1/module4labs.py
@@ -90,6 +90,37 @@ for i in range(1, 20):
 	if is_prime(i + 1):
 			print(i + 1, end=" ")
 print()
+
+"""
+Europe: fuel consumed per 100 kilometers
+USA: miles travelled using 1 gallon of fuel
+convert 
+"""
+#1 mile = 1609.344 metres
+#1 gallon = 3.785411784 litres
+
+def liters_100km_to_miles_gallon(litres):
+	miles = None
+	gallons = litres / 3.785411784
+	miles = 100000 / 1609.344
+	
+	return miles / gallons
+
+def miles_gallon_to_liters_100km(miles):
+	litres = None
+	metres = miles * 1609.344
+	km = metres / 1000
+	litres = 3.785411784
+
+	return (litres * 100)/km
+
+print(liters_100km_to_miles_gallon(3.9))
+print(liters_100km_to_miles_gallon(7.5))
+print(liters_100km_to_miles_gallon(10.))
+print(miles_gallon_to_liters_100km(60.3))
+print(miles_gallon_to_liters_100km(31.4))
+print(miles_gallon_to_liters_100km(23.5))
+
  
 
 
