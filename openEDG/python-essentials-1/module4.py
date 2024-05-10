e5d24b300354f41594ecc7a521cb396954699d79
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-10 09:10:24 +0200

    module 4

--- openEDG/python-essentials-1/module4.py
+++ openEDG/python-essentials-1/module4.py
@@ -66,3 +66,6 @@ except ValueError:
     print('I do not know what to do.')    
 except ZeroDivisionError:
     print('Division by zero is not allowed in our Universe.') 
+except:
+    print("Something strange has happened here ... Sorry!")
+# default exception should go last, always
