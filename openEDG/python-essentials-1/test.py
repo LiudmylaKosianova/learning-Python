f6e390aece2e61ef7c5bca36bee36f82f455ef5a
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-12 08:08:12 +0200

    python essentials 2 module 1

--- openEDG/python-essentials-1/test.py
+++ openEDG/python-essentials-1/test.py
@@ -1,5 +1,25 @@
-for i in range(-1,1):
-    print("#")
+# for i in range(-1,1):
+#     print("#")
 
-my_list = [3, 1, -2]
-print(my_list[my_list[-1]])
+# my_list = [3, 1, -2]
+# print(my_list[my_list[-1]])
+
+# a = 1
+# def plus(a):
+#     return a
+# try:
+#     a = 'abbc'
+# except:
+#     print("Something went wrong")
+# terracota = [101, 102, 103, 104, 105]
+
+# def terracota(terracota):
+#     del terracota[3]
+#     terracota[3] = 0
+# print(terracota(terracota))
+ 
+x = 3
+y = 2
+wow = x%y
+print(1%2)
+    
---
+++ openEDG/python-essentials-2/module1.py
@@ -0,0 +1,11 @@
+import math
+# from math import* (not safe, imports all the functions sin())
+# from math import sin (convenient, but can cause the conflict sin())
+# import math as m ( then the ivocation will be: m.sin())
+# from math import sin as SINE (invocation will be: SINE())
+
+# sin from math and sin created here are not in conflict
+print(math.sin(math.pi/2))
+def sin(x):
+    print("This is handmade sin function")
+sin(1)
