64cc86d8f873799fafe425021b690d3671f0f514
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-13 15:48:00 +0200

    module 1

--- openEDG/python-essentials-2/module1.py
+++ openEDG/python-essentials-2/module1.py
@@ -5,8 +5,19 @@ import math
 # from math import sin as SINE (invocation will be: SINE())
 
 # sin from math and sin created here are not in conflict
-print(math.sin(math.pi/2))
-def sin(x):
-    print("This is handmade sin function")
-sin(1)
-print(dir(math))
+# print(math.sin(math.pi/2))
+# def sin(x):
+#     print("This is handmade sin function")
+# sin(1)
+# print(dir(math))
+from platform import platform
+from platform import processor
+from platform import system
+
+print(platform())
+print(platform(1))
+print(platform(0, 1))
+print(processor())
+print(system())
+
+
