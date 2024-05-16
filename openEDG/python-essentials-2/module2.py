a5085ad4b99c226a310af1f0439e2ac2c6174fef
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-16 12:27:41 +0200

    python essentials 2 module 2

---
+++ openEDG/python-essentials-2/module2.py
@@ -0,0 +1,16 @@
+multipleLine = '''line n 1
+line n 2'''
+print(len(multipleLine))
+a = 'a'
+b = 'b'
+a2 = a*2
+ab = a + b
+print(a, b, a2, ab)
+
+# I can check the code for a character
+
+char1 = 'F'
+char2 = 'r'
+char3 = 'Ф' # Ukrainian character 
+print(ord(char1))
+print(ord(char3))
