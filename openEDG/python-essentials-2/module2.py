fdefcc830eb7d2e64f961b5790f03cf93b35183d
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-16 12:41:12 +0200

    module 2

--- openEDG/python-essentials-2/module2.py
+++ openEDG/python-essentials-2/module2.py
@@ -7,10 +7,28 @@ a2 = a*2
 ab = a + b
 print(a, b, a2, ab)
 
-# I can check the code for a character
+# I can check the code of a character
 
 char1 = 'F'
-char2 = 'r'
-char3 = 'Ф' # Ukrainian character 
+char2 = 'Ф' # Ukrainian character 
 print(ord(char1))
-print(ord(char3))
+print(ord(char2))
+
+point1 = 70
+point2 = 1060
+print(chr(point1))
+print(chr(point2))
+print(chr(ord('F')) == 'F')
+
+# Slices
+
+alpha = "abdefg"
+
+print(alpha[1:3])
+print(alpha[3:])
+print(alpha[:3])
+print(alpha[3:-2])
+print(alpha[-3:4])
+print(alpha[::2])
+print(alpha[1::2])
+
