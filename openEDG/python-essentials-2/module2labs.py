fa8cacc69e9c055eaf8383570034a2e3a4ca9931
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-17 13:31:04 +0200

    module 2 labs

--- openEDG/python-essentials-2/module2labs.py
+++ openEDG/python-essentials-2/module2labs.py
@@ -5,19 +5,19 @@ def mysplit(strng):
     mylist = []
     if len(strng) == 0 or strng.isspace():
         return mylist
-    
+    #print(f"strng length: {len(strng)}")
+    strng = strng.strip()
+    strng = strng + ' '
     listElement = ""
+    #print(f"strng length: {len(strng)}")
     for char in strng:
-        if char != 32:
-            listElement =listElement + char
+        if char != ' ':
+            listElement = listElement + char
         else:
             mylist.append(listElement)
-            listElement = ""
+            listElement = ""  
+      
     return mylist
-        
-
-    
-        
 
 
 print(mysplit("To be or not to be, that is the question"))
