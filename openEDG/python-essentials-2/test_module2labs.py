a1b4049311e202f807a918a02d29333af89f3959
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-17 13:38:50 +0200

    module 2 labs and a test

--- openEDG/python-essentials-2/test_module2labs.py
+++ openEDG/python-essentials-2/test_module2labs.py
@@ -1,7 +1,9 @@
 from module2labs import mysplit
+import pytest
 
+@pytest.mark.lab1
 def test_mysplit():
     assert mysplit("") == []
     assert mysplit("   ") == []
-    #assert mysplit("cherry is red") == ["cherry", "is", "red"]
+    assert mysplit("cherry is red") == ["cherry", "is", "red"]
     assert mysplit("   abc   ") == ["abc"]
