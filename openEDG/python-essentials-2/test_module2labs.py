fba17c2957f4dc0298df1240eb72beb8a8dd41ae
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-19 12:42:39 +0200

    module 2 labs tests

--- openEDG/python-essentials-2/test_module2labs.py
+++ openEDG/python-essentials-2/test_module2labs.py
@@ -1,5 +1,6 @@
-from module2labs import mysplit
 import pytest
+from module2labs import mysplit
+from module2labs import ledInputCheck
 
 @pytest.mark.lab1
 def test_mysplit():
@@ -7,3 +8,11 @@ def test_mysplit():
     assert mysplit("   ") == []
     assert mysplit("cherry is red") == ["cherry", "is", "red"]
     assert mysplit("   abc   ") == ["abc"]
+
+@pytest.mark.lab2
+def test_ledInputCheck(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _:"-7")
+    assert ledInputCheck() == -1
+    
+
+
