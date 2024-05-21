a3adb9808e516a06baf7a81b00b7e95a1827e4c8
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-21 15:14:22 +0200

    hidden not regarding order

--- openEDG/python-essentials-2/module2labs.py
+++ openEDG/python-essentials-2/module2labs.py
@@ -227,5 +227,32 @@ def digit(birthdate):
             digit += int(char)
     return digit
 
+
+"""
+Scenario
+Let's play a game. 
+We will give you two strings: 
+one being a word (e.g., "dog") and the second being a combination of any characters.
+
+Your task is to write a program which answers the following question: 
+are the characters comprising the first string hidden inside the second string?
+
+For example:
+
+if the second string is given as "vcxzxduybfdsobywuefgas", the answer is yes;
+if the second string is "vcxzxdcybfdstbywuefsas", the answer is no 
+(as there are neither the letters "d", "o", or "g", in this order)
+"""
+
+def hidden(word, combo):
+    
+    for char in word:
+        if char not in combo:
+            return "no"
+    
+    return "yes"
+    
+
+
 #caesarCypher("The die is cast", 25)
 #caesarCypherInput("Hello")
