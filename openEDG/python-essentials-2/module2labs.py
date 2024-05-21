5b31a6afa2a8f70981f46a8b5622f21695099d26
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-21 14:06:58 +0200

    module 2 labs digit of life

--- openEDG/python-essentials-2/module2labs.py
+++ openEDG/python-essentials-2/module2labs.py
@@ -203,7 +203,29 @@ def anagrams(line1, line2):
     else:
         return di1 == di2
 
- 
+"""
+Scenario
+Some say that the Digit of Life is a digit evaluated using somebody's birthday. 
+It's simple - you just need to sum all the digits of the date. 
+If the result contains more than one digit, 
+you have to repeat the addition until you get exactly one digit. 
+For example:
+
+1 January 2017 = 2017 01 01
+2 + 0 + 1 + 7 + 0 + 1 + 0 + 1 = 12
+1 + 2 = 3
+3 is the digit we searched for and found.
+"""
+def digit(birthdate):
+    digit = 0
+    for char in birthdate:
+        digit += int(char)
+    while digit > 9:
+        strDigit = str(digit)
+        digit = 0
+        for char in strDigit:
+            digit += int(char)
+    return digit
 
 #caesarCypher("The die is cast", 25)
 #caesarCypherInput("Hello")
