002d3434e8f328b456b46810ade34defbe7c0ffa
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-24 14:23:09 +0200

    stack

--- openEDG/python-essentials-2/module3_stack.py
+++ openEDG/python-essentials-2/module3_stack.py
@@ -1,6 +1,15 @@
 class Stack:  # Defining the Stack class.
     def __init__(self):  # Defining the constructor function.
-        print("Hi!")
+        self.stackList = []
 
 
-stack_object = Stack()  # Instantiating the object.
+s_object = Stack()  # Instantiating the object.
+print(len(s_object.stackList))
+
+class Stack2:  # Defining the Stack class.
+    def __init__(self):  # Defining the constructor function.
+        self.__stackList = [] #Now it is private, cannot be accessed from outside
+
+
+s_object2 = Stack2()  # Instantiating the object.
+print(len(s_object.__stackList))
