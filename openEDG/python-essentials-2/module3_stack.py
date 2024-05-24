a440b5330b76e1e862b74cf45e44e8c0e777627a
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-24 17:29:41 +0200

    stack

--- openEDG/python-essentials-2/module3_stack.py
+++ openEDG/python-essentials-2/module3_stack.py
@@ -1,15 +1,32 @@
-class Stack:  # Defining the Stack class.
+class Stack0:  # Defining the Stack class.
     def __init__(self):  # Defining the constructor function.
         self.stackList = []
 
 
-s_object = Stack()  # Instantiating the object.
-print(len(s_object.stackList))
+s_object0 = Stack0()  # Instantiating the object.
+print(len(s_object0.stackList))
 
-class Stack2:  # Defining the Stack class.
+class Stack:  # Defining the Stack class.
     def __init__(self):  # Defining the constructor function.
         self.__stackList = [] #Now it is private, cannot be accessed from outside
 
+    def push(self, val):
+        self.__stackList.append(val)
 
-s_object2 = Stack2()  # Instantiating the object.
-print(len(s_object.__stackList))
+    def pop(self):
+        val = self.__stackList[-1]
+        del self.__stackList[-1]
+        return val
+
+
+#s_object = Stack()  # Instantiating the object.
+#print(len(s_object.__stackList))#will raise an exception 
+stack_object = Stack()
+
+stack_object.push(3)
+stack_object.push(2)
+stack_object.push(1)
+
+print(stack_object.pop())
+print(stack_object.pop())
+print(stack_object.pop())
