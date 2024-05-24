1e7151369884ab3c6b609e359832c134b019f5ab
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-24 20:40:57 +0200

    stack

--- openEDG/python-essentials-2/module3_stack.py
+++ openEDG/python-essentials-2/module3_stack.py
@@ -30,3 +30,31 @@ stack_object.push(1)
 print(stack_object.pop())
 print(stack_object.pop())
 print(stack_object.pop())
+
+class AddingStack(Stack):
+    def __init__(self):
+        Stack.__init__(self)
+        self.__sum = 0
+    
+    def push(self, val):
+        self.__sum += val
+        Stack.push(self, val)
+
+    def pop(self):
+        val = Stack.pop(self)
+        self.__sum -= val
+        return val
+    
+    def get_sum(self):
+        return self.__sum
+    
+print("***AddingStack***")
+    
+stack_object = AddingStack()
+
+for i in range(5):
+    stack_object.push(i)
+print(stack_object.get_sum())
+
+for i in range(5):
+    print(stack_object.pop())
