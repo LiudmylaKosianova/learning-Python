3818eefb18ce6c65b25ac44501ff1baa92f701ef
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-25 14:38:51 +0200

    module 3

--- openEDG/python-essentials-2/module3.py
+++ openEDG/python-essentials-2/module3.py
@@ -24,3 +24,66 @@ push(1)
 print(pop())
 print(pop())
 print(pop())
+"""
+instance variables
+"""
+# class ExampleClass:
+#     def __init__(self, val = 1):
+#         self.first = val
+
+#     def set_second(self, val):
+#         self.second = val
+
+
+# example_object_1 = ExampleClass()
+# example_object_2 = ExampleClass(2)
+
+# example_object_2.set_second(3)
+
+# example_object_3 = ExampleClass(4)
+# example_object_3.third = 5
+
+# print(example_object_1.__dict__)
+# print(example_object_2.__dict__)
+# print(example_object_3.__dict__)
+
+# class ExampleClass:
+#     def __init__(self, val = 1):
+#         self.__first = val
+
+#     def set_second(self, val = 2):
+#         self.__second = val
+
+
+# example_object_1 = ExampleClass()
+# example_object_2 = ExampleClass(2)
+
+# example_object_2.set_second(3)
+
+# example_object_3 = ExampleClass(4)
+# example_object_3.__third = 5
+
+
+# print(example_object_1.__dict__)
+# print(example_object_2.__dict__)
+# print(example_object_3.__dict__)
+
+"""
+class variables
+"""
+
+class ExampleClass:
+    __counter = 0
+    def __init__(self, val = 1):
+        self.__first = val
+        ExampleClass.__counter += 1
+
+
+example_object_1 = ExampleClass()
+example_object_2 = ExampleClass(2)
+example_object_3 = ExampleClass(4)
+
+print(example_object_1.__dict__, example_object_1._ExampleClass__counter)
+print(example_object_2.__dict__, example_object_2._ExampleClass__counter)
+print(example_object_3.__dict__, example_object_3._ExampleClass__counter)
+
