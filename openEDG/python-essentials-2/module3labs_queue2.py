5ceb4507ebc9b1edf48dc690a36227613ca7e2db
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-25 10:34:11 +0200

    queue 2

--- openEDG/python-essentials-2/module3labs_queue2.py
+++ openEDG/python-essentials-2/module3labs_queue2.py
@@ -5,17 +5,39 @@ We want it to have a parameterless method that returns
 True if the queue is empty and False otherwise.
 """
 
-class QueueError(???):
+class QueueError(IndexError):
     pass
 
 
 class Queue:
+    def __init__(self):
+        self.__list = []
+
+    def put(self, elem):
+        self.__list.insert(0, elem)
+
+    def get(self):
+        try:
+            element = self.__list[-1]
+            del self.__list[-1]
+            return element
+        except:
+            raise QueueError
+    def get_list(self):
+        return self.__list
     #
     # Code from the previous lab.
     #
 
 
 class SuperQueue(Queue):
+    def __init__(self):
+        Queue.__init__(self)
+    
+    def isempty(self):
+        return self.get_list == []
+
+    
     #
     # Write new code here.
     #
