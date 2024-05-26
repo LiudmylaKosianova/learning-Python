bb442d5c35d548b1419887fd96ed62de4f3174fc
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-26 18:29:39 +0200

    module 3

--- openEDG/python-essentials-2/module3.py
+++ openEDG/python-essentials-2/module3.py
@@ -72,18 +72,93 @@ instance variables
 class variables
 """
 
+# class ExampleClass:
+#     __counter = 0
+#     def __init__(self, val = 1):
+#         self.__first = val
+#         ExampleClass.__counter += 1
+
+
+# example_object_1 = ExampleClass()
+# example_object_2 = ExampleClass(2)
+# example_object_3 = ExampleClass(4)
+
+# print(example_object_1.__dict__, example_object_1._ExampleClass__counter)
+# print(example_object_2.__dict__, example_object_2._ExampleClass__counter)
+# print(example_object_3.__dict__, example_object_3._ExampleClass__counter)
+
+"""
+__dict__
+"""
+# class ExampleClass:
+#     varia = 1
+#     def __init__(self, val):
+#         ExampleClass.varia = val
+
+# print("***")
+# print(ExampleClass.__dict__)
+# example_object = ExampleClass(2)
+# print("*")
+# print(ExampleClass.__dict__)
+# print("*")
+# print(example_object.__dict__)
+
+"""
+check if instance has an attribute
+"""
+
 class ExampleClass:
-    __counter = 0
-    def __init__(self, val = 1):
-        self.__first = val
-        ExampleClass.__counter += 1
+    redSofa = 99
+    def __init__(self, val):
+        if val % 2 != 0:
+            self.a = 1
+        else:
+            self.b = 1
 
 
-example_object_1 = ExampleClass()
-example_object_2 = ExampleClass(2)
-example_object_3 = ExampleClass(4)
+example_object = ExampleClass(1)
+print(example_object.a)
 
-print(example_object_1.__dict__, example_object_1._ExampleClass__counter)
-print(example_object_2.__dict__, example_object_2._ExampleClass__counter)
-print(example_object_3.__dict__, example_object_3._ExampleClass__counter)
+if hasattr(example_object, 'b'):
+    print(example_object.b)
+print(hasattr(ExampleClass, "redSofa"))
+
+"""
+class methods
+"""
+class Classy:
+    varia = 2
+    def method(self):
+        print(self.varia, self.var)
+
+
+obj = Classy()
+obj.var = 3
+obj.method()
+
+class Classy:
+    def visible(self):
+        print("visible")
+    
+    def __hidden(self):
+        print("hidden")
+
+
+obj = Classy()
+obj.visible()
+
+try:
+    obj.__hidden()
+except:
+    print("failed")
+
+obj._Classy__hidden()
+
+class Classy:
+    pass
+
+
+print(Classy.__name__)
+obj = Classy()
+print(type(obj).__name__)
 
